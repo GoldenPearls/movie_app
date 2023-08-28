805eebde17b8877ee272e81360c9f71faa043504
[GoldenPearls] <[prettylee620@naver.com]>
2023-08-28 16:23:15 +0900

    [design] 사진, 지역 추가 1. 사진 지역 추가 2. rating 추가 수정 전

--- src/App.js
+++ src/App.js
@@ -1,61 +1,73 @@
 import React from "react";
 
-function Food({ name, picture, rating }) {
+function Festival({ name, region, picture, rating }) {
   //app에서 보낸 인자를 받아보기
   //{ fav } = props;
+
   return (
     <div>
-      <h2> I like {name} </h2>
+      <h2> {name} </h2>
+      <h3> 지역 : {region} </h3>
       <h4>{rating}/5.0</h4>
       <img src={picture} alt={name}></img>
     </div>
   );
 }
 
-const foodILike = [
+<i className="item-rating pointer zmdi zmdi-star-outline"></i>;
+<i className="item-rating pointer zmdi zmdi-star-outline"> </i>;
+const festivalILike = [
   {
     id: 1,
-    name: "kimchi",
+    name: "머드 축제",
+    region: "충청남도 보령",
     image:
-      "https://recipe1.ezmember.co.kr/cache/recipe/2020/10/14/954d0e47cad6ce0f6bda194b5a4075b21.jpg",
+      "http://www.mudfestival.or.kr/template/festival/user/images/sub/history_img01.jpg",
     rating: 5,
   },
   {
     id: 2,
-    name: "ramen",
-    image:
-      "https://health.chosun.com/site/data/img_dir/2022/09/23/2022092301908_0.jpg",
+    name: "벚꽃 축제",
+    region: "서울 여의도 ",
+    image: "https://i.ytimg.com/vi/mHxZQSF1W6o/maxresdefault.jpg",
     rating: 4.9,
   },
   {
     id: 3,
-    name: "jeyuk bokkeum",
-    image:
-      "https://recipe1.ezmember.co.kr/cache/recipe/2017/07/01/7ef9cedcbe5cceb5d3b17c81b305a9c21.jpg",
+    name: "서울 세계 불꽃 축제",
+    region: "서울 여의도 ",
+    image: "https://img.sbs.co.kr/newimg/news/20170924/201097006_1280.jpg",
     rating: 4.5,
   },
   {
     id: 4,
-    name: "kimbab",
+    name: "예산 맥주페스티벌",
+    region: "충청남도 예산 ",
     image:
-      "https://recipe1.ezmember.co.kr/cache/recipe/2015/04/04/0461907459756bc3a56472da407a1a9d1.jpg",
+      "https://image.edaily.co.kr/images/photo/files/NP/S/2023/08/PS23082500285.jpg",
     rating: 3.8,
   },
 ];
 
-function renderFood(dish) {
+function renderFestival(festival) {
   return (
-    <Food
-      key={dish.id}
-      name={dish.name}
-      picture={dish.image}
-      rating={dish.rating}
+    <Festival
+      key={festival.id}
+      name={festival.name}
+      region={festival.region}
+      picture={festival.image}
+      rating={festival.rating}
     />
   );
 }
 
 function App() {
-  return <div>{foodILike.map((dish) => renderFood(dish))}</div>;
+  return (
+    <div>
+      <h1>축제 소개</h1>
+      {festivalILike.map((festival) => renderFestival(festival))}
+    </div>
+  );
 }
 
 export default App;
