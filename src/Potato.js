3648b89274f8abd22737eed4b9d1ed5595f8d10c
[GoldenPearls] <[prettylee620@naver.com]>
2023-08-28 13:39:21 +0900

    [test]Potato 컴포넌트 삭제해보기

--- src/Potato.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-function Potato() {
-  //Potato 컴포넌트의 기본틀 중요한 규칙 : 컴포넌트 정의 시 첫글자 대문자로 시작
-  return <h3>I love React!</h3>;
-}
-
-export default Potato; //export default 이름
